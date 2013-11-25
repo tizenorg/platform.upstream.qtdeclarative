899d48883deabf2dfd24d099c44c4438150d802d
Tomasz Olszak <olszak.tomasz@gmail.com>
2013-11-25 22:07:59 +0100

    Workaround for Tizen emulator like for Nouveau driver.
    Index buffer object is broken on Tizen Emulator due to some
    translation layer between host GPU and qemu.
    
    Change-Id: I427fc71391c05c1a5513f42532f2d82595cb4893
    Reviewed-by: Jarosław Staniek <staniek@kde.org>

--- src/quick/scenegraph/qsgcontext.cpp
+++ src/quick/scenegraph/qsgcontext.cpp
@@ -449,7 +449,7 @@ void QSGRenderContext::initialize(QOpenGLContext *context)
     m_gl->setProperty(QSG_RENDERCONTEXT_PROPERTY, QVariant::fromValue(this));
     m_sg->renderContextInitialized(this);
 
-#ifdef Q_OS_LINUX
+#if defined(Q_OS_LINUX) && !defined(Q_OS_LINUX_TIZEN_SIMULATOR)
     const char *vendor = (const char *) glGetString(GL_VENDOR);
     if (strstr(vendor, "nouveau"))
         m_brokenIBOs = true;
@@ -458,6 +458,10 @@ void QSGRenderContext::initialize(QOpenGLContext *context)
         m_serializedRender = true;
     if (strstr(vendor, "Hisilicon Technologies") && strstr(renderer, "Immersion.16"))
         m_brokenIBOs = true;
+#endif
+
+#ifdef Q_OS_LINUX_TIZEN_SIMULATOR
+    m_brokenIBOs = true;
 #endif
 
     emit initialized();
