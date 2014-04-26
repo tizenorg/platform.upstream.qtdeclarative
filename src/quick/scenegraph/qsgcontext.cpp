d7373b546246fdc7c967cb8d6af93e4c08b64e1d
Tomasz Olszak <olszak.tomasz@gmail.com>
2014-04-26 21:56:09 +0200

    Tizen: Use QT_TIZEN_EMULATOR macro
    Instead of deprecated Q_OS_LINUX_TIZEN_SIMULATOR.
    
    Change-Id: I9de03126807ac3f32248594e63201234f7020b1e
    Reviewed-by: Leon Anavi <leon@anavi.org>
    Reviewed-by: Tomasz Olszak <olszak.tomasz@gmail.com>

--- src/quick/scenegraph/qsgcontext.cpp
+++ src/quick/scenegraph/qsgcontext.cpp
@@ -449,7 +449,7 @@ void QSGRenderContext::initialize(QOpenGLContext *context)
     m_gl->setProperty(QSG_RENDERCONTEXT_PROPERTY, QVariant::fromValue(this));
     m_sg->renderContextInitialized(this);
 
-#if defined(Q_OS_LINUX) && !defined(Q_OS_LINUX_TIZEN_SIMULATOR)
+#if defined(Q_OS_LINUX) && !defined(QT_TIZEN_EMULATOR)
     const char *vendor = (const char *) glGetString(GL_VENDOR);
     if (strstr(vendor, "nouveau"))
         m_brokenIBOs = true;
@@ -460,7 +460,7 @@ void QSGRenderContext::initialize(QOpenGLContext *context)
         m_brokenIBOs = true;
 #endif
 
-#ifdef Q_OS_LINUX_TIZEN_SIMULATOR
+#ifdef QT_TIZEN_EMULATOR
     m_brokenIBOs = true;
 #endif
 
